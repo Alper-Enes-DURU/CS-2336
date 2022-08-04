cb5796201567a1a1b093b30d6ea5258a2a54c16c
alperduru <enesduru@gmail.com>
2022-08-04 17:04:16 -0500

    Initialize the repository

---
+++ Computer Science 2/src/Lecture_2.java
@@ -0,0 +1,14 @@
+// test case: {35, 30, 0, -45}
+
+public class Lecture_2 {
+
+	public static void main(String[] args) {
+		if (i > k)
+		{
+			if (j > k)
+				System.out.println("");
+		}
+
+	}
+
+}
